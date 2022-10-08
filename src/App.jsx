530e71b29eae886d25f2c709af791c97fa326c92
Virus <amitkeshri003@gmail.com>
2022-10-08 18:32:27 +0530

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 //import { useState } from 'react'
 //import reactLogo from './assets/react.svg'
 import './App.css'
+import Footer from './assets/footer/footer'
 
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Footer/>
     </div>
   )
 }
