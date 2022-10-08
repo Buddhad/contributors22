f92055c429530422440c45b2e7f677fb22ee7483
Buddhadeb Chhetri <buddhadebchhetri6@gmail.com>
2022-10-08 16:43:03 +0500

    Fist commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default App
