0c4cbc4f382929765175f0019bf3c63a3f9578e4
VEDANG <it.developer2002@gmail.com>
2022-10-13 20:49:24 +0530

    changes in app.jsx heading tag

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@
 //import reactLogo from './assets/react.svg'
 import './App.css'
 import Card from "./components/Card"
+// typed npm package added
+import Typed from 'react-typed' 
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -9,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">
-      HelloOctober World!
+      <Typed  className='md:text-5xl sm:text-4xl font-bold pl-3' strings={['Hey folks','Welcome to','Hacktoberfest 2022']} typeSpeed={120} backSpeed={140} loop/>
     </h1>
     <Card />
     </div>
