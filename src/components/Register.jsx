077719fb7c8df9527a0dd3f326fb089ef3acd9b8
VEDANG <it.developer2002@gmail.com>
2022-10-18 13:34:49 +0530

    Responsive hamburger bar + sign in signup page ui

---
+++ src/components/Register.jsx
@@ -0,0 +1,107 @@
+import React from 'react'
+import {useNavigate} from 'react-router-dom'
+const Register = () => {
+    
+    const navigate=useNavigate();
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        alert('registered successfully')
+    
+        // 👇️ redirect to /contacts
+        navigate('/login');
+      };
+
+
+
+
+
+  return (
+    <div className=''>
+        <section class="h-screen">
+  <div class="px-6 h-full text-gray-800">
+    <div
+      class="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6"
+    >
+      <div
+        class="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0"
+      >
+        <img
+          src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+          class="w-full"
+          alt="Sample image"
+        />
+      </div>
+      <div class="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
+        <form onSubmit={handleSubmit} method='post'>
+          <div class="flex flex-row items-center justify-center lg:justify-start">
+            <p class="text-lg  mr-4 mb-5 font-extrabold">Register</p>
+           
+            
+          </div>
+
+          
+
+          <div class="mb-6">
+            <input
+              type="text"
+              class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              id="exampleFormControlInput2"
+              placeholder="Name"
+            />
+          </div>
+
+          <div class="mb-6">
+            <input
+              type="Number"
+              class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              id="exampleFormControlInput2"
+              placeholder="Number"
+            />
+          </div>
+
+         
+          <div class="mb-6">
+            <input
+              type="text"
+              class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              id="exampleFormControlInput2"
+              placeholder="Email address"
+            />
+          </div>
+
+          
+          <div class="mb-6">
+            <input
+              type="password"
+              class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              id="exampleFormControlInput2"
+              placeholder="Password"
+            />
+          </div>
+
+         
+
+          <div class="text-center lg:text-left">
+            <button
+              type="submit"
+              class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            >
+              Register
+            </button>
+            
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
+
+  
+    
+    
+    </div>
+  )
+}
+
+export default Register
