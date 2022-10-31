f3cd2ce5ea4a2abd7e92c2c1775c27feab451183
Naman <75575170+Naman-Singla777@users.noreply.github.com>
2022-11-01 00:47:54 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,40 +1,29 @@
-import React from 'react';
-import octocat from '/images/hula_loop_octodex03.gif';
-import { BsGithub } from 'react-icons/bs';
+import React from 'react'
 
-const Footer = () => {
+export const About = () => {
   return (
-    <>
-      <footer class="text-white body-font bg-black">
-        <div class="px-5 py-4 mx-auto flex items-center sm:flex-row flex-col">
-          <a class="flex flex-col sm:flex-row title-font font-medium items-center md:justify-start justify-center text-gray-900">
-            <img src={octocat} class="w-auto h-16 text-white rounded-full" />
-            <span class="text-xl text-white mx-4">Contributors</span>
-          </a>
-          <p class="text-sm text-white-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-            © 2022 Hacktober Contributors —
-            <a
-              href="https://hacktoberfest.com"
-              class="text-white-600 ml-1"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              @hacktoberfest
-            </a>
-          </p>
-          <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-            <a
-              href="https://github.com/Buddhad/contributors22"
-              target="_blank"
-              className="text-gray-500 cursor-pointer"
-            >
-              <BsGithub className="h-6 w-6" />
-            </a>
-          </span>
-        </div>
-      </footer>
-    </>
-  );
-};
-
-export default Footer;
+    <div>Hello, About
+      <section className="text-gray-600 body-font">
+  <div className="container px-5 py-24 mx-auto flex flex-wrap items-center">
+    <div className="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
+      <h1 className="title-font font-medium text-3xl text-gray-900">Slow-carb next level shoindcgoitch ethical authentic, poko scenester</h1>
+      <p className="leading-relaxed mt-4">Poke slow-carb mixtape knausgaard, typewriter street art gentrify hammock starladder roathse. Craies vegan tousled etsy austin.</p>
+    </div>
+    <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
+      <h2 className="text-gray-900 text-lg font-medium title-font mb-5">Sign Up</h2>
+      <div className="relative mb-4">
+        <label for="full-name" className="leading-7 text-sm text-gray-600">Full Name</label>
+        <input type="text" id="full-name" name="full-name" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+      </div>
+      <div className="relative mb-4">
+        <label for="email" className="leading-7 text-sm text-gray-600">Email</label>
+        <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+      </div>
+      <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+      <p className="text-xs text-gray-500 mt-3">Literally you probably haven't heard of them jean shorts.</p>
+    </div>
+  </div>
+</section>
+    </div>
+  )
+}
